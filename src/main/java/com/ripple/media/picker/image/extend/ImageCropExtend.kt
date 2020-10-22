8111c595982ed940aa4e23b1192af7057617413f
fanyafeng <fanyafeng@live.cn>
2020-10-22 09:43:31 +0800

    Author: fanyafeng Email: fanyafeng@live.cn Modify: 1.添加图片裁剪功能

---
+++ src/main/java/com/ripple/media/picker/image/extend/ImageCropExtend.kt
@@ -0,0 +1,21 @@
+package com.ripple.media.picker.image.extend
+
+import android.content.Context
+import com.ripple.media.picker.config.CropImageConfig
+
+/**
+ * Author: fanyafeng
+ * Date: 2020/10/22 09:14
+ * Email: fanyafeng@live.cn
+ * Description: 图片裁剪扩展方法
+ *
+ * 图片裁剪分为两种：
+ * 第一种：直接塞进一张图片进行裁剪
+ * 第二种：在图库中选择之后进入到裁剪页面进行裁剪
+ *
+ * 这个扩展类中只包含第一种
+ */
+
+fun Context.imageCrop(localImagePath:String,cropImageConfig: CropImageConfig) {
+
+}
